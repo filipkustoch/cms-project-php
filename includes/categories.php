40f0e93f4bfb10ebca7dc68c8d04ad1f6fc2d87b
filipkustoch <filip.kustoch1@gmail.com>
2023-01-10 19:45:24 +0100

    added Categories widget and Admin button at navbar

--- includes/categories.php
+++ includes/categories.php
@@ -3,17 +3,16 @@
     <div class="card-body">
         <div class="row">
             <div class="col-sm-6">
+                <?php
+                $query = "SELECT * FROM categories LIMIT 3";
+                $select_all_categories_query = mysqli_query($connection, $query); ?>
+
                 <ul class="list-unstyled mb-0">
-                    <li><a href="#!">Web Design</a></li>
-                    <li><a href="#!">HTML</a></li>
-                    <li><a href="#!">Freebies</a></li>
-                </ul>
-            </div>
-            <div class="col-sm-6">
-                <ul class="list-unstyled mb-0">
-                    <li><a href="#!">JavaScript</a></li>
-                    <li><a href="#!">CSS</a></li>
-                    <li><a href="#!">Tutorials</a></li>
+
+                    <?php while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                        $cat_title = $row['title']; ?>
+                        <li><a href="#!"><?php echo $cat_title ?></a></li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
