f6d2c912d92aa2ee4fceed468ba38fa251258132
filipkustoch <filip.kustoch1@gmail.com>
2023-01-28 00:30:36 +0100

    Creating users pages and links

---
+++ admin/includes/view_all_users.php
@@ -0,0 +1,26 @@
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Post</th>
+            <th>Author</th>
+            <th>Email</th>
+            <th>Content</th>
+            <th>Status</th>
+            <th>Date</th>
+            <th>Approve</th>
+            <th>Disapprove</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php draw_table_comments(); ?>
+    </tbody>
+</table>
+
+<?php
+approve_comment();
+disapprove_comment();
+delete_comment();
+?>
