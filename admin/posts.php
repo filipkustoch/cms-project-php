378c8dad13aa77e88675f3b1d233c41c999811fb
filipkustoch <filip.kustoch1@gmail.com>
2023-02-27 01:53:12 +0100

    Select all working and added jquery

--- admin/posts.php
+++ admin/posts.php
@@ -57,9 +57,11 @@
         <!-- /#page-wrapper -->
     </div>
     <!-- /#wrapper -->
-    <script src="js/scripts.js"></script>
+    
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
+
+    <script src="js/scripts.js"></script>
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
