542916de3cc7cd7d37b2657d920a47e1449e042a
filipkustoch <filip.kustoch1@gmail.com>
2023-03-03 23:48:58 +0100

    Added dynamic personalizaton to Admin

--- admin/includes/admin_navbar.php
+++ admin/includes/admin_navbar.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?> 
+
 <!-- Brand and toggle get grouped for better mobile display -->
 <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
@@ -40,7 +44,7 @@
         </ul>
     </li>
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['username']; ?> <b class="caret"></b></a>
         <ul class="dropdown-menu">
             <li>
                 <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
