178359052b2b0c01b4f885aa7624d36e782b8bdd
filipkustoch <filip.kustoch1@gmail.com>
2023-03-20 23:06:11 +0100

    Relating Posts to their authors h2 added

--- author_posts.php
+++ author_posts.php
@@ -14,8 +14,11 @@ include "includes/head.php"; ?>
                 if (isset($_GET['p_id'])) {
                     $post_id = $_GET['p_id'];
                     $post_author = $_GET['author'];
-                }
-
+                } ?>
+                <h2>All posts by
+                    <?php echo $post_author ?>
+                </h2>
+                <?php
                 $query = "SELECT * FROM posts WHERE author = '{$post_author}'";
                 $select_post = mysqli_query($connection, $query);
                 while ($row = mysqli_fetch_assoc($select_post)) {
@@ -28,13 +31,22 @@ include "includes/head.php"; ?>
                     $post_image = $row['image']; ?>
 
                     <div class='card mb-4'>
-                        <a href='post.php?p_id=<?php echo $post_id ?>'><img class='card-img-top post-image' src='images/<?php echo $post_image ?>' alt='...' /></a>
+                        <a href='post.php?p_id=<?php echo $post_id ?>'><img class='card-img-top post-image'
+                                src='images/<?php echo $post_image ?>' alt='...' /></a>
                         <div class='card-body'>
-                            <div class='small text-muted'><?php echo $post_date ?></div>
-                            <h2 class='card-title h4'><?php echo $post_title ?></h2>
-                            <p class='card-text'><?php echo $post_content ?></p>
+                            <div class='small text-muted'>
+                                <?php echo $post_date ?>
+                            </div>
+                            <h2 class='card-title h4'>
+                                <?php echo $post_title ?>
+                            </h2>
+                            <p class='card-text'>
+                                <?php echo $post_content ?>
+                            </p>
                             <a class='btn btn-primary' href='post.php?p_id=<?php echo $post_id ?>'>Read more →</a>
-                            <p class='lead'><a href='author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id ?>' style='text-decoration: none;'>@<?php echo $post_author ?></a></p>
+                            <p class='lead'><a
+                                    href='author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id ?>'
+                                    style='text-decoration: none;'>@<?php echo $post_author ?></a></p>
                         </div>
                     </div>
                 <?php } ?>
